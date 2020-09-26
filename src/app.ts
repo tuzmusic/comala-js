7e882ba5c9f97332aade1f8f9876d6a852aafabd
Jonathan Tuzman <tuzmusic@gmail.com>
2020-09-26 13:00:49 -0400

    Set up ts server and tests, clean up files

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,5 @@
-console.log("Hello world");
+console.log('Hello world!!! blah');
+
+export function testFunc() {
+  return 42
+}
--- test/app.test.ts
+++ test/app.test.ts
@@ -1,9 +1,7 @@
-import request from "supertest";
-import app from "../src/app";
+import { testFunc } from '../src/app';
 
-describe("GET /random-url", () => {
-    it("should return 404", (done) => {
-        request(app).get("/reset")
-            .expect(404, done);
+describe("app", () => {
+    it("should return 42", () => {
+      expect(testFunc()).toEqual(42)
     });
 });
