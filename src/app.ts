b09079809e5dd22120bc8fb01f04362aacdc594f
Jonathan Tuzman <tuzmusic@gmail.com>
2020-09-26 14:02:34 -0400

    Complete basic tag creation

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,42 @@
-export function createTag(name: string): string {
-  let result = '';
-  const tag = `{${ name }}`;
-  result = [tag, tag].join('\n');
+console.log('Hello app');
+
+interface TagOptions {
+  closing?: boolean;
+}
+
+const defaultOptions: TagOptions = {
+  closing: true
+};
+
+export function createTag(
+  name: string,
+  parameters: Record<string, string | boolean> = {}, // TODO: catalog parameters in a type
+  body: string | null = null,
+  options: TagOptions = defaultOptions
+): string {
   
-  return result;
+  function getOpeningTag() {
+    let openingTag = '{' + name;
+    
+    for (const key in parameters) {
+      if (parameters.hasOwnProperty(key)) { // really shouldn't be necessary but typescript complains
+        openingTag += (key == Object.keys(parameters)[0] ? ':' : '|');
+        if (key !== 'unnamed') openingTag += `${ key }=`;
+        openingTag += `${ parameters[key] }`;
+      }
+    }
+    
+    return openingTag + '}';
+  }
+  
+  const lines = [];
+  lines.push(getOpeningTag());
+  
+  if (body) lines.push('\t' + body);
+  
+  if (options.closing) lines.push(`{${ name }}`);
+  
+  const fullTag = lines.join('\n');
+  
+  return fullTag;
 }
--- test/app.test.ts
+++ test/app.test.ts
@@ -2,7 +2,67 @@ import { createTag } from '../src/app';
 
 describe('createTag', () => {
   it('creates a bare tag', () => {
-    const res = createTag('workflow');
-    expect(res).toEqual(['{workflow}', '{workflow}'].join('\n'));
+    const actual = createTag('workflow');
+    expect(actual).toEqual(['{workflow}', '{workflow}'].join('\n'));
+  });
+  
+  it('has the option for a non-closing tag', () => {
+    expect(createTag('approval', {}, null, { closing: false })).toEqual('{approval}');
+  });
+  
+  it('can have parameters', () => {
+    const actual = createTag('workflow', { name: 'Some Workflow', label: 'some-label' });
+    const expected = [
+      '{workflow:name=Some Workflow|label=some-label}',
+      '{workflow}'
+    ].join('\n');
+    expect(actual).toEqual(expected);
+  });
+  
+  it('can have an unnamed parameter', () => {
+    const actual = createTag('state', { unnamed: 'In Progress', label: 'some-label' });
+    const expected = [
+      '{state:In Progress|label=some-label}',
+      '{state}'
+    ].join('\n');
+    expect(actual).toEqual(expected);
+  });
+  
+  it('can have a boolean parameter', () => {
+    const actual = createTag('approval', { unnamed: 'Review', assignable: true }, null, { closing: false });
+    const expected = '{approval:Review|assignable=true}';
+    expect(actual).toEqual(expected);
+  });
+  
+  it('can do parameters, and options', () => {
+    const actual = createTag('state', { unnamed: 'In Progress', label: 'some-label' }, null, { closing: false });
+    const expected = '{state:In Progress|label=some-label}';
+    expect(actual).toEqual(expected);
+  });
+  
+  it('can have a text body', () => {
+    const str = 'Label added. Editing should be restricted to the creator only.';
+    const actual = createTag('set-message', { user: '@user@' }, str);
+    const expected = [
+      '{set-message:user=@user@}',
+      '\tLabel added. Editing should be restricted to the creator only.',
+      '{set-message}',
+    ].join('\n');
+    expect(actual).toEqual(expected);
+  });
+  
+  it('can have a tag body (which is actually just a text body!)', () => {
+    const innerTag = createTag('approval', { unnamed: 'Review', assignable: true }, null, { closing: false });
+    const outerTag = createTag(
+      'state',
+      { unnamed: 'In Progress', approved: 'Approved', taskable: true },
+      innerTag
+    );
+    const expected = [
+      '{state:In Progress|approved=Approved|taskable=true}',
+      '\t{approval:Review|assignable=true}',
+      '{state}',
+    ].join('\n');
+    expect(outerTag).toEqual(expected);
   });
 });
