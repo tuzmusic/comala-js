f5eaf2e6c934b5d51fb332eda24b15a5091417cc
Jonathan Tuzman <tuzmusic@gmail.com>
2020-10-05 18:13:01 -0400

    Scrape the list of macros

--- src/scraper/scraper.ts
+++ src/scraper/scraper.ts
@@ -1,6 +1,8 @@
 import cheerio from 'cheerio';
 import fs from 'fs';
+import request from 'request';
 
+//region Types
 type ScrapeTarget = {
   className: string;
   url: string;
@@ -24,12 +26,26 @@ type DocObject = {
   required?: boolean;
   unnamed?: boolean;
 }
+//endregion
 
-function scrape(): ScrapedObject[] {
-  const htmlUrl = '/Users/tuzmacbookpro2017/dev/MyProjects/comala-js/test/fixtures/state-macro.html';
-  const content = fs.readFileSync(htmlUrl, { encoding: 'utf8', flag: 'r' });
+// Async version of request function
+function asyncRequest(url: string): Promise<{ response: any; html: string }> {
+  return new Promise((resolve, reject) => {
+    request(url, (error, response, html) => {
+      if (error) reject(error);
+      resolve({ response, html });
+    });
+  });
+}
+
+async function scrape(url: string): Promise<ScrapedObject[]> {
+  // if (!url) {
+  //   const htmlUrl = '/Users/tuzmacbookpro2017/dev/MyProjects/comala-js/test/fixtures/state-macro.html';
+  //   const content = fs.readFileSync(htmlUrl, { encoding: 'utf8', flag: 'r' });
+  // }
+  const { html } = await asyncRequest(url);
   
-  const $ = cheerio.load(content);
+  const $ = cheerio.load(html);
   // todo: make sure that it's the right table.
   const table = $('table.confluenceTable').first();
   const rows = $('tr', table);
@@ -84,7 +100,16 @@ function constructParam({ parameterName, defaultValue, notes, required, version 
   if (editedParam.required)
     doc.push(' * This parameter is REQUIRED.');
   
-  doc.push(' *', notes.html);
+  // TODO: use cheerio to actually modify the html how we want it
+  // we might even use this to convert to JSDoc markup instead of
+  // html
+  // notes
+  doc.push(
+    ' * ' +
+    notes.html
+      .replace(/\n+/g, '')// replace newlines
+      .replace(/  +/g, ' ') // reduce spaces
+  );
   
   // declaration
   doc.push(' */', `${ editedParam.name }: ${ editedParam.type };\n`,);
@@ -92,15 +117,15 @@ function constructParam({ parameterName, defaultValue, notes, required, version 
   return doc;
 }
 
-function createFile(macro: ScrapeTarget) {
+async function createFile(macro: ScrapeTarget) {
   const fileLines = [
     'import Tag from \'./Tag\'',
     '',
     `class ${ macro.className } extends Tag {`,
   ];
-  const allParameters = scrape();
+  const allParameters = scrape(macro.url);
   // console.log(allParameters)
-  const textLines: string[] = allParameters.map(constructParam).flat();
+  const textLines: string[] = (await allParameters).map(constructParam).flat();
   
   textLines.forEach(line => fileLines.push('\t' + line));
   
@@ -111,7 +136,58 @@ function createFile(macro: ScrapeTarget) {
   const path = `/Users/tuzmacbookpro2017/dev/MyProjects/comala-js/src/${ macro.className }.ts`;
   
   fs.writeFileSync(path, str);
-  // console.log(fs.readFileSync(path, 'utf8'));
+  console.log(fs.readFileSync(path, 'utf8'));
 }
 
-createFile({ className: 'State', url: 'https://wiki.comalatech.com/display/CDML/state+macro#' });
+const macros: ScrapeTarget[] = [
+  { className: 'State', url: 'https://wiki.comalatech.com/display/CDML/state+macro#' }
+];
+
+async function getAllMacros() {
+  const { html } = await asyncRequest('https://wiki.comalatech.com/display/CDML/Macros');
+  const $ = cheerio.load(html);
+  const headings = $('h2', '.contentLayout2');
+  
+  const macroLists: {
+    name: string;
+    macros: ScrapeTarget[];
+  }[] = [];
+  
+  const baseUrl = 'https://wiki.comalatech';
+  
+  headings.each(i => {
+    const heading = headings.eq(i);
+    if (heading.text() === 'Other') return
+    
+    const macroListObject = { name: heading.text(), macros: [] as ScrapeTarget[] };
+    
+    // find the list
+    let ul;
+    let current = heading.next();
+    
+    while (!ul) {
+      const tagName: string = current.prop('tagName');
+      if (tagName.toLowerCase() === 'ul')
+        ul = current;
+      else
+        current = current.next()
+    }
+    
+    // scrape the list
+    const macrosUnderHeading = $('a', ul);
+    macrosUnderHeading.each(j => {
+      const link = macrosUnderHeading.eq(j);
+      macroListObject.macros.push({
+        className: link.text().replace(' macro', ''), url: baseUrl + link.attr('href')
+      });
+    });
+    
+    // add the list of macros to our list of... lists!
+    macroLists.push(macroListObject);
+  });
+  console.log(macroLists);
+}
+
+getAllMacros();
+
+// createFile({ className: 'State', url: 'https://wiki.comalatech.com/display/CDML/state+macro#' });
