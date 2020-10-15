5a4b9eb8a3fa61e79bcbce28f364983588c9585f
Jonathan Tuzman <tuzmusic@gmail.com>
2020-10-15 14:33:29 -0400

    Implement fast-track rejection

--- src/declarative-api/classes/WorkflowCreator.ts
+++ src/declarative-api/classes/WorkflowCreator.ts
@@ -46,25 +46,25 @@ export default class WorkflowCreator {
   // approvals: Record<string, Tag> = {};
   states: Tag[] = [];
   triggers: Tag[] = [];
-
+  
   constructor(obj: WorkflowObject) {
     this.workflow = new Tag('workflow', { name: obj.name, label: obj.label });
     const { workflow } = this;
-
+    
     this.states = obj.states.map(this.processState); // populates this.state
     this.states.forEach(workflow.addChild);
     this.triggers.forEach(workflow.addChild);
-
+    
     console.log(workflow.markup);
   };
-
+  
   private processState = (stateObj: StateObject): Tag => {
     // create the basic state tag
     const stateTag = new Tag('state', { _: stateObj.name });
-
+    
     // some simple parameters
     addParamsFromObjectToTag(['final', 'hideSelection'], stateObj, stateTag);
-
+    
     // add the parameters for (simple) transitions
     const transitions = { 'onApproved': 'approved' };
     Object.entries(transitions).forEach(([key, value]) => {
@@ -72,22 +72,22 @@ export default class WorkflowCreator {
       if (nextState)
         stateTag.addParameter({ [value]: nextState as string });
     });
-
+    
     // create the approvals
     stateObj.approvals?.map((approvalObj: ApprovalObject) =>
       this.processApproval(approvalObj, stateObj),
     ).forEach(stateTag.addChild);
-
+    
     // set the state permissions
     this.manageStatePermissions(stateObj);
-
+    
     return stateTag;
   };
-
+  
   private manageStatePermissions = (stateObj: StateObject) => {
     const { name: stateName, permissions } = stateObj;
     if (!permissions) return;
-
+    
     // prepare the trigger tag
     const triggerTag = new Tag('trigger', { _: 'statechanged', state: stateName });
     // add the set-restrictions tags
@@ -95,12 +95,12 @@ export default class WorkflowCreator {
     // add to workflow
     this.triggers.push(triggerTag);
   };
-
+  
   private addPermissionsToTrigger = (permissions: PermissionsGroup, triggerTag: Tag) => {
     permissionsTypes.forEach(type => {
       if (permissions[type]) {
         const tag = new Tag('set-restrictions', { type }, true);
-
+  
         // add empty-group, or add groups/users
         if (userTypes.every(key => !permissions[type][key].length)) {
           tag.addParameter({ group: 'empty-group' });
@@ -114,25 +114,21 @@ export default class WorkflowCreator {
       }
     });
   };
-
+  
   private manageReviewerPermissions = (approvalObj: ApprovalObject, stateObj: StateObject) => {
     const { name: approvalName, reviewersCan } = approvalObj;
     if (!reviewersCan) return;
-    const triggers = [
-      new Tag('trigger', { _: 'pageapprovalassigned', approval: approvalName }),
-      new Tag('trigger', { _: 'approvalunassigned', approval: approvalName }),
-    ];
-
+    
     // make a copy of the state's permissions
     const statePermissions = { ...stateObj.permissions };
-
+    
     // add reviewers to the state's permissions
     permissionsTypes.forEach(key => {
       if (reviewersCan[key]) {
         statePermissions[key].users.push('@approvalassignees@');
       }
     });
-
+    
     ['pageapprovalassigned', 'approvalunassigned']
       .forEach(name => {
           const trigger = new Tag('trigger', { _: name, approval: approvalName });
@@ -141,16 +137,16 @@ export default class WorkflowCreator {
         },
       );
   };
-
+  
   private processApproval = (approvalObj: ApprovalObject, stateObj: StateObject): Tag => {
     // create the basic approval tag
     const approvalTag = new Tag('approval', { _: approvalObj.name });
-
+    
     // some simple parameters
     addParamsFromObjectToTag(['rememberAssignees'], approvalObj, approvalTag);
-
-    const { allowedAssigners, allowedApprovers } = approvalObj;
-
+    
+    const { allowedAssigners, allowedApprovers, fastReject } = approvalObj;
+    
     // set who can assign
     if (allowedAssigners) {
       if (allowedAssigners.groups?.length)
@@ -158,21 +154,46 @@ export default class WorkflowCreator {
       if (allowedAssigners.users?.length)
         approvalTag.addParameter({ allowedassignusers: allowedAssigners.users.join(',') });
     }
-
+    
     // set who can be assigned
     if (allowedApprovers) {
       const approvers: string[] = allowedApprovers.groups.concat(allowedApprovers.users);
       if (approvers.length)
         approvalTag.addParameter({ selectedapprovers: approvers.join(',') });
     }
-
+    
+    // handle fast-track rejection
+    if (fastReject) {
+      const trigger = new Tag('trigger', {
+        _: 'pagerejected',
+        approval: approvalObj.name,
+        partial: 'true'
+      });
+      trigger.addChild(new Tag('set-state', { _: fastReject }, true));
+      
+      // find the set-state trigger for the state the fastReject moves to
+      const statePermissionTrigger = this.triggers.find(
+        tag => tag.parameters['_'] === 'statechanged'
+          && tag.parameters['state'] === fastReject
+      );
+      
+      // that trigger sets the permissions. grab the set-restrictions tags.
+      const permissions = statePermissionTrigger.children.filter(
+        tag => tag.tagName === 'set-restrictions'
+      );
+      
+      // copy those tags to our current trigger.
+      permissions.forEach(trigger.addChild);
+      this.triggers.push(trigger)
+    }
+    
     // handle tasks
     for (const { name, assignee, completeOn } of approvalObj.tasks) {
       const taskTag = new Tag('task', { name, assignee }, true);
-
+      
       approvalTag.addChild(taskTag);
     } // for (task of tasks)
-
+    
     this.manageReviewerPermissions(approvalObj, stateObj);
     return approvalTag;
   };
