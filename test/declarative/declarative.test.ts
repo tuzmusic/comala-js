294079895f412cc06389384d801d8f8cff030dd2
Jonathan Tuzman <tuzmusic@gmail.com>
2020-10-15 14:17:40 -0400

    Add test for fast-track rejection, including permissions

--- test/declarative/declarative.test.ts
+++ test/declarative/declarative.test.ts
@@ -6,7 +6,7 @@ const { inside } = new RegexChainer();
 
 describe('createWorkflow', () => {
   const { workflow } = new WorkflowCreator(workflowObj as WorkflowObject);
-
+  
   const states = {
     InProgress: { stateNamed: 'In Progress' },
     InApproval: { stateNamed: 'In Approval' },
@@ -16,36 +16,40 @@ describe('createWorkflow', () => {
     Editing: { approvalNamed: 'Audit Editing' },
     Review: { approvalNamed: 'Audit Review' },
   };
-
+  
   const inWorkflow = inside(workflow.markup);
-
+  
   it('has the workflow tag', () => {
     inWorkflow
       .expect({ tagNamed: 'workflow' })
       .toHaveParam({ name: 'Audit Workflow with Assigned Editing' })
       .toHaveParam({ label: 'workflow=internal-audit-record-editassign' });
   });
-
+  
+  it('Doesn\'t have any empty parameters', () => {
+    expect(workflow.markup).not.toMatch(/=[|}]/);
+  });
+  
   describe('States', () => {
     it('has the basic state tags', () => {
       inWorkflow.expect(states.InProgress)
         .toComeBefore(states.InApproval)
         .toComeBefore(states.Published);
     });
-
+    
     it('Assigns the "approved" parameter', () => {
       inWorkflow.expect(states.InProgress)
         .toComeBefore({ approved: 'In Approval' })
         .and(states.InApproval)
         .toComeBefore({ approved: 'Published' });
     });
-
+    
     it('adds simple parameters to the state tag', () => {
       inWorkflow.expect(states.Published)
         .toHaveParam({ final: 'true' })
         .toHaveParam({ hideSelection: 'true' });
     });
-
+    
     describe('Permissions', () => {
       test('Permissions tags are self-closing', () => {
         inWorkflow.expect('{set-restrictions}').not.toOccur();
@@ -56,27 +60,29 @@ describe('createWorkflow', () => {
             .toHaveParam({ state: states.InProgress.stateNamed })
             .toHaveChild({ tagNamed: 'set-restrictions' })
             .withParam({ type: 'view' })
-            .andParam({ group: 'Internal Audit Managers,Internal Audit Team' })
-          // .not.toInclude('|user=');
-
+            .andParam({ group: 'Internal Audit Managers,Internal Audit Team' });
+  
           inWorkflow.expect({ triggerNamed: 'statechanged' })
             .toHaveParam({ state: states.InProgress.stateNamed })
             .toHaveChild({ tagNamed: 'set-restrictions' })
             .withParam({ type: 'edit' })
             .andParam({ group: 'Internal Audit Managers,Internal Audit Team' });
         });
-
-        xit('Leaves out empty array parameters', () => {
-          inWorkflow.expect({ triggerNamed: 'statechanged' }).not.toInclude({ user: '' });
-        });
-
+        /*
+        * 
+        {trigger:pagerejected|approval=Audit Review|partial=true}
+          {set-state:In Progress}
+          {set-restrictions:type=view|group=Internal Audit Managers,Internal Audit Team}
+          {set-restrictions:type=edit|group=Internal Audit Managers,Internal Audit Team}
+        {trigger}
+    * */
         xit('Sets the permissions when we re-enter the state from a rejection.', () => {
           // NOTE: We do need to do this because the 'statechanged' trigger
           // is not activated by the fast-reject trigger (because statechanged
           // doesn't fire in response to triggers).
         });
       });
-
+      
       describe('In Approval state', () => {
         it('Sets the view permissions upon entering the state', () => {
           inWorkflow.expect({ triggerNamed: 'statechanged' })
@@ -85,7 +91,7 @@ describe('createWorkflow', () => {
             .withParam({ type: 'view' })
             .andParam({ group: 'Internal Audit Managers,Internal Audit Team' });
         });
-
+        
         it('Sets the edit permissions (no one!) upon entering the state', () => {
           inWorkflow.expect({ triggerNamed: 'statechanged' })
             .toHaveParam({ state: states.InApproval.stateNamed })
@@ -93,7 +99,7 @@ describe('createWorkflow', () => {
             .withParam({ type: 'edit' })
             .andParam({ group: 'empty-group' });
         });
-
+        
         describe('Reviewer permissions', () => {
           it('Gives a reviewer view permissions when they are added', () => {
             inWorkflow.expect({ triggerNamed: 'pageapprovalassigned' })
@@ -125,7 +131,7 @@ describe('createWorkflow', () => {
       });
     });
   });
-
+  
   describe('Approvals', () => {
     it('Creates the approval tags', () => {
       inWorkflow.expect(states.InProgress)
@@ -133,30 +139,56 @@ describe('createWorkflow', () => {
         .and(states.InApproval)
         .toHaveChild(approvals.Review);
     });
-
+    
     it('adds simple parameters to the approval tag', () => {
       inWorkflow.expect(approvals.Editing).toHaveParam({ rememberAssignees: true })
         .and(approvals.Review).toHaveParam({ rememberAssignees: true });
     });
-
+    
     it('Designates who is allowed to assign approvers', () => {
       inWorkflow.expect(approvals.Editing)
         .toHaveParam({ allowedassigngroups: 'Internal Audit Managers' })
         .and(approvals.Review)
         .toHaveParam({ allowedassigngroups: 'Internal Audit Managers' });
     });
-
+    
     it('Leaves out empty array parameters', () => {
       inWorkflow.expect(approvals.Editing).not.toInclude('allowedassignusers');
     });
-
+    
     it('Designates who is allowed to be assigned to approvals', () => {
       inWorkflow.expect(approvals.Editing)
         .toHaveParam({ selectedapprovers: 'Internal Audit Managers,Internal Audit Team' })
         .and(states.InApproval)
         .toHaveParam({ selectedapprovers: 'SLI Internal,jt-audit-manager' });
     });
-
+    
+    /*
+    *  {trigger:pagerejected|approval=Audit Review|partial=true}
+      {set-state:In Progress}
+      {set-restrictions:type=view|group=Internal Audit Managers,Internal Audit Team}
+      {set-restrictions:type=edit|group=Internal Audit Managers,Internal Audit Team}
+  {trigger}*/
+    it('Defines fast-track rejections', () => {
+      inWorkflow.expect({ triggerNamed: 'pagerejected' })
+        .toHaveParam({ approval: approvals.Review.approvalNamed })
+        .andParam({ partial: 'true' })
+        .toInclude('{set-state:In Progress}')
+    });
+    
+    it('Restores permissions on fast-tracked rejections', () => {
+      inWorkflow.expect({ triggerNamed: 'pagerejected' })
+        .toHaveParam({ approval: approvals.Review.approvalNamed })
+        .toHaveChild({ tagNamed: 'set-restrictions' })
+        .withParam({ type: 'edit' })
+        .andParam({ group: 'Internal Audit Managers,Internal Audit Team' })
+      inWorkflow.expect({ triggerNamed: 'pagerejected' })
+        .toHaveParam({ approval: approvals.Review.approvalNamed })
+        .toHaveChild({ tagNamed: 'set-restrictions' })
+        .withParam({ type: 'view' })
+        .andParam({ group: 'Internal Audit Managers,Internal Audit Team' })
+    });
+    
     describe('Tasks on approvals', () => {
       it('Adds the task in the approval', () => {
         inWorkflow.expect(approvals.Editing)
@@ -168,11 +200,11 @@ describe('createWorkflow', () => {
           .withParam({ name: 'Assign reviewers' })
           .andParam({ assignee: '@author@' });
       });
-
+      
       test('Tasks are self-closing', () => {
         inWorkflow.expect('{task}').not.toOccur();
       });
-
+      
       xit('Completes the task when appropriate', () => {
         // todo
       });
