c1fae5cd32e50f8c3111fc07367c57b83a5e76f8
Jonathan Tuzman <jtuzman@sunriselabs.com>
2020-10-15 10:12:27 -0400

    Extract processApproval

--- src/declarative-api/classes/WorkflowCreator.ts
+++ src/declarative-api/classes/WorkflowCreator.ts
@@ -2,6 +2,7 @@ import Tag from '../../Tag';
 
 type WorkflowObject = any
 type StateObject = any
+type ApprovalObject = any
 
 function addParamsFromObjectToTag(camelCaseParams: string[], object: any, tag: Tag) {
   camelCaseParams.forEach(param => {
@@ -14,22 +15,23 @@ function addParamsFromObjectToTag(camelCaseParams: string[], object: any, tag: T
 
 export default class WorkflowCreator {
   workflow: Tag;
-  states: Record<string, Tag> = {};
+  // states: Record<string, Tag> = {};
+  // approvals: Record<string, Tag> = {};
+  states: Tag[];
 
   constructor(obj: WorkflowObject) {
     this.workflow = new Tag('workflow', { name: obj.name, label: obj.label });
     const { workflow } = this;
 
-    this.states = obj.states.map(this.processState);
-    Object.values(this.states).forEach(workflow.addChild);
+    this.states = obj.states.map(this.processState); // populates this.state
+    this.states.forEach(workflow.addChild);
 
     console.log(workflow.markup);
   };
 
-  processState = (stateObj: StateObject): Tag => {
+  private processState = (stateObj: StateObject): Tag => {
     // create the basic state tag
     const stateTag = new Tag('state', { _: stateObj.name });
-    this.states[stateObj.name] = stateTag;
 
     // some simple parameters
     addParamsFromObjectToTag(['final', 'hideSelection'], stateObj, stateTag);
@@ -42,41 +44,45 @@ export default class WorkflowCreator {
         stateTag.addParameter({ [value]: nextState });
     });
 
-    for (const approvalObj of stateObj.approvals ?? []) {
-      // create the basic approval tag
-      const approvalTag = new Tag('approval', { _: approvalObj.name });
-
-      // some simple parameters
-      addParamsFromObjectToTag(['rememberAssignees'], approvalObj, approvalTag);
-
-      const { allowedAssigners, allowedApprovers } = approvalObj;
-
-      // set who can assign
-      if (allowedAssigners) {
-        if (allowedAssigners.groups?.length)
-          approvalTag.addParameter({ allowedassigngroups: allowedAssigners.groups.join(',') });
-        if (allowedAssigners.users?.length)
-          approvalTag.addParameter({ allowedassignusers: allowedAssigners.users.join(',') });
-      }
-
-      // set who can be assigned
-      if (allowedApprovers) {
-        const approvers: string[] = allowedApprovers.groups.concat(allowedApprovers.users);
-        if (approvers.length)
-          approvalTag.addParameter({ selectedapprovers: approvers.join(',') });
-      }
-
-      // handle tasks
-      for (const { name, assignee, completeOn } of approvalObj.tasks) {
-        const taskTag = new Tag('task', { name, assignee });
-
-        approvalTag.addChild(taskTag);
-      } // for (task of tasks)
-
-      stateTag.addChild(approvalTag);
-    } // for (approval of approvals)
+    stateObj.approvals?.map(this.processApproval)
+      .forEach(stateTag.addChild);
 
     return stateTag;
+  };
+
+  private processApproval = (approvalObj: ApprovalObject): Tag => {
+    // create the basic approval tag
+    const approvalTag = new Tag('approval', { _: approvalObj.name });
+
+    // some simple parameters
+    addParamsFromObjectToTag(['rememberAssignees'], approvalObj, approvalTag);
+
+    const { allowedAssigners, allowedApprovers } = approvalObj;
+
+    // set who can assign
+    if (allowedAssigners) {
+      if (allowedAssigners.groups?.length)
+        approvalTag.addParameter({ allowedassigngroups: allowedAssigners.groups.join(',') });
+      if (allowedAssigners.users?.length)
+        approvalTag.addParameter({ allowedassignusers: allowedAssigners.users.join(',') });
+    }
+
+    // set who can be assigned
+    if (allowedApprovers) {
+      const approvers: string[] = allowedApprovers.groups.concat(allowedApprovers.users);
+      if (approvers.length)
+        approvalTag.addParameter({ selectedapprovers: approvers.join(',') });
+    }
+
+    // handle tasks
+    for (const { name, assignee, completeOn } of approvalObj.tasks) {
+      const taskTag = new Tag('task', { name, assignee });
+
+      approvalTag.addChild(taskTag);
+    } // for (task of tasks)
+
+    // stateTag.addChild(approvalTag);
+    return approvalTag;
   };
 }
 
