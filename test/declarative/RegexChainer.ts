c2c87d2da0442141563d5c3623f62173e84b3289
Jonathan Tuzman <tuzmusic@gmail.com>
2020-10-16 10:22:13 -0400

    Implement new getRelevantInfo (or whatever it's called)

--- test/declarative/RegexChainer.ts
+++ test/declarative/RegexChainer.ts
@@ -8,6 +8,8 @@ type FlexibleArgs =
 
 type StringFunc = (args: FlexibleArgs) => RegexChainer;
 
+type TagInfo = { [tagType: string]: { name: string } | null }
+
 // TODO: Checking children and params needs to make sure the search ends at
 // the right place. Otherwise all we're doing is checking if Y exists *anywhere*
 // after X. e.g., a child of tag Z could test positive as a child of tag X.
@@ -15,8 +17,11 @@ type StringFunc = (args: FlexibleArgs) => RegexChainer;
 
 export default class RegexChainer {
   source: string;
-  stored: string;
-
+  stored = {} as
+    { type: 'tag'; content: TagInfo }
+    | { type: 'param'; content: object }
+    | { type: 'string'; content: string }
+  
   // these aliases are defined at the end of the file
   // because referring to the functions, defined below, gives
   // a TS error, despite the es-lint setting at the top.
@@ -29,64 +34,37 @@ export default class RegexChainer {
   andExpect: StringFunc;
   and: StringFunc;
   private isNot: boolean;
-
+  
   constructor() {
     this.isNot = false;
-    this.toHaveParam =
-      this.withParam =
-        this.andParam =
-          this.toHaveChild =  // todo: and check closing tag?
-            this.andHaveChild =  // todo: and check closing tag?
-              this.toInclude = this.toComeBefore;
+    // this.toHaveParam =
+    //   this.withParam =
+    //     this.andParam =
+    //       this.toHaveChild =  // todo: and check closing tag?
+    //         this.andHaveChild =  // todo: and check closing tag?
+    //           this.toInclude = this.toComeBefore;
     this.andExpect =
       this.and = this.expect;
   };
-
+  
   get not() {
     this.isNot = true;
     return this;
   }
-
-  private get reset() {
-    this.stored = this.source = null;
+  
+  expect = (args: FlexibleArgs) => {
     this.isNot = false;
+    this.getRelevantString(args);
     return this;
-  }
-
-  private static getRelevantString(args: FlexibleArgs): string {
-    let str;
-    if (typeof args === 'string') { // string
-      str = args as string;
-    } else {
-      ['state', 'approval', 'trigger'].forEach(name => {
-        const key = name + 'Named';
-        const value = (args as { [key: string]: string })[key];
-        if (value) args = { tagNamed: name, unnamed: value };
-      });
-
-      if (args.hasOwnProperty('tagNamed')) { // object with tagName property
-        str = '{' + args.tagNamed + (args.unnamed ? `:${ args.unnamed }` : '');
-      } else { // PARAMS: object without tagName property. treat as an object of params
-        // todo: this currently only work for named parameters
-        str = ('[:|]' + Object.keys(args)[0].toLowerCase() + '=' + Object.values(args)[0] + '[|}]');
-      }
-    }
-    return str.replace(/([|=])/g, '\\$1');
-  }
-
+  };
+  
   inside = (s: string) => {
     // aliases
     this.stored = null;
     this.source = s;
     return this;
   };
-
-  expect = (args: FlexibleArgs) => {
-    this.isNot = false;
-    this.stored = RegexChainer.getRelevantString(args);
-    return this;
-  };
-
+  
   test = (str: string) => {
     const { isNot, source } = this;
     const regExp = new RegExp(str, 's');
@@ -96,31 +74,69 @@ export default class RegexChainer {
       expect(source).toMatch(regExp);
     }
   };
-
-  toOccur = () => {
-    this.test(RegexChainer.getRelevantString(this.stored));
-    return this;
-  };
-
-  toComeBefore: StringFunc = args => {
-    const { source, stored } = this;
-    if (!source || !stored) throw new Error('Arguments missing.');
-
-    const matchStr = `${ stored }.*${ RegexChainer.getRelevantString(args) }`;
-    this.test(matchStr);
-
-    return this;
-  };
-
-  toComeAfter: StringFunc = args => {
-    const { stored } = this;
-    if (!stored) throw new Error('Arguments missing.');
-    const oldStored = stored;
-    this.stored = RegexChainer.getRelevantString(args);
-    this.toComeBefore(oldStored);
-
-    return this;
-  };
+  
+  private getRelevantString(args: FlexibleArgs): void {
+    // handle simple string argument
+    if (typeof args === 'string') { // string
+      this.stored = { type: 'string', content: prepareForRegex(args) };
+      return;
+    }
+    
+    // handle specific tags
+    for (const typeOfTag of ['state', 'approval', 'trigger', 'tag']) {
+      const key = typeOfTag + 'Named';
+      // check if this is the key
+      const value: string = (args as Record<string, string>)[key];
+      if (value) {
+        // tagNamed: 'xyz' is {xyz...
+        // stateNamed: 'abc" is {state:abc
+        const content = typeOfTag === 'tag'
+          ? { [value]: { name: null } }
+          : { [typeOfTag]: { name: value } };
+        this.stored = { type: 'tag', content };
+        return;
+      }
+    }
+    
+    // if we've come this far we have an object with unknown keys
+    this.stored = { type: 'param', content: args };
+    
+    /*
+    if (args.hasOwnProperty('tagNamed')) { // object with tagName property
+      str = '{' + args.tagNamed + (args.unnamed ? `:${ args.unnamed }` : '');
+    } else { // PARAMS: object without tagName property. treat as an object of params
+      // todo: this currently only work for named parameters
+      str = ('[:|]' + Object.keys(args)[0].toLowerCase() + '=' + Object.values(args)[0] + '[|}]');
+    }*/
+  }
+  
+  /*  toOccur = () => {
+      this.test(RegexChainer.getRelevantString(this.stored));
+      return this;
+    };
+    
+    toComeBefore: StringFunc = args => {
+      const { source, stored } = this;
+      if (!source || !stored) throw new Error('Arguments missing.');
+      
+      const matchStr = `${ stored }.*${ RegexChainer.getRelevantString(args) }`;
+      this.test(matchStr);
+      
+      return this;
+    };
+    
+    toComeAfter: StringFunc = args => {
+      const { stored } = this;
+      if (!stored) throw new Error('Arguments missing.');
+      const oldStored = stored;
+      this.stored = RegexChainer.getRelevantString(args);
+      this.toComeBefore(oldStored);
+      
+      return this;
+    };
+    */
 }
+
+const prepareForRegex = (str: string) => str.replace(/([|=])/g, '\\$1');
 
 
--- test/declarative/regexChainer.test.ts
+++ test/declarative/regexChainer.test.ts
@@ -1,8 +1,50 @@
 import RegexChainer from './RegexChainer';
 
+const chainer = new RegexChainer();
+const { inside } = chainer;
+/*
 test('test works', () => {
-  const { inside } = new RegexChainer();
-
   inside('apples and :fruit=bananas').expect('apples').toComeBefore({ fruit: 'bananas' });
   inside('{apples and bananas').expect('bananas').toComeAfter({ tagNamed: 'apples' });
+});
+*/
+
+describe('real tag testing', () => {
+  describe('expect', () => {
+    const source = inside('whatever');
+    test('type: string', () => {
+      source.expect('string');
+      expect(chainer.stored).toEqual({ type: 'string', content: 'string' });
+    });
+    
+    test('known tag', () => {
+      source.expect({ stateNamed: 'state1' });
+      expect(chainer.stored).toEqual({ type: 'tag', content: { state: { name: 'state1' } } });
+      source.expect({ approvalNamed: 'approval1' });
+      expect(chainer.stored).toEqual({ type: 'tag', content: { approval: { name: 'approval1' } } });
+      source.expect({ triggerNamed: 'trigger1' });
+      expect(chainer.stored).toEqual({ type: 'tag', content: { trigger: { name: 'trigger1' } } });
+    });
+    
+    test('unknown tag', () => {
+      source.expect({ tagNamed: 'task' });
+      expect(chainer.stored).toEqual({ type: 'tag', content: { task: { name: null } } });
+    });
+    
+    test('param', () => {
+      source.expect({ name: 'something' });
+      expect(chainer.stored).toEqual({ type: 'param', content: { name: 'something' } });
+    });
+  });
+  test('params', () => {
+    const source = '{state:Name1|param1=value1}' +
+      '{state}' +
+      '{state:Name2|param2=value2}' +
+      '{state}';
+    
+    inside(source).expect({ stateNamed: 'Name1' }).toHaveParam({ param1: 'value1' });
+    inside(source).expect({ stateNamed: 'Name1' }).not.toHaveParam({ param2: 'value2' });
+    inside(source).expect({ stateNamed: 'Name2' }).not.toHaveParam({ param1: 'value1' });
+    inside(source).expect({ stateNamed: 'Name2' }).toHaveParam({ param2: 'value2' });
+  });
 });
